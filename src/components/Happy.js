ba8f2ee68da75b53f2ffe10bb39651da89f7ec79
Tim Marchant <fromtimwithcode@gmail.com>
2021-08-09 12:12:13 -0500

    Template for Happy.js

--- src/components/Happy.js
+++ src/components/Happy.js
@@ -1,2 +1,14 @@
+// Step1: import React
 import React from 'react';
 
+// Step2: function declaration
+// (can be either function declaration OR function expression)
+function Happy() {
+    // Step 3: return something...
+    return (
+
+    )
+}
+
+//Step 4: export!
+export default Happy;
